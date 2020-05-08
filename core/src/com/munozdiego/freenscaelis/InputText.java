2b1c8b7afb7aaed749df39e4acb883e2c0127973
Diego Muñoz Jorquera <munoz.jorquera.diego@alumnat.copernic.cat>
2020-05-08 16:56:31 +0200

    Added utils

--- core/src/com/munozdiego/freenscaelis/InputText.java
+++ core/src/com/munozdiego/freenscaelis/InputText.java
@@ -15,17 +15,28 @@ import com.badlogic.gdx.InputAdapter;
  */
 public class InputText extends InputAdapter {
 
+  private int limit;
   private String text = "";
   private boolean capsEnabled;
 
   public InputText() {
     super();
+    limit = 200;
     capsEnabled = false;
   }
-  
-  public String getText() { return text; }
-  public void resetText() { text = ""; }
-  
+
+  public String getText() {
+    return text;
+  }
+
+  public void resetText() {
+    text = "";
+  }
+
+  public void setLimit(int l) {
+    limit = l;
+  }
+
   @Override
   public boolean keyDown(int keyCode) {
     if (MyGame.DEBUG_MODE) {
@@ -37,231 +48,233 @@ public class InputText extends InputAdapter {
     }
     if (Input.Keys.DEL == keyCode) {
       if (text.length() > 0) {
-        if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT)){
+        if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT)) {
           text = "";
         } else {
           text = text.substring(0, text.length() - 1);
         }
       }
     } else {
-      if (capsEnabled) {
+      if (text.length() <= limit) {
+        if (capsEnabled) {
+          switch (keyCode) {
+            case Input.Keys.A:
+              text += "A";
+              break;
+            case Input.Keys.B:
+              text += "B";
+              break;
+            case Input.Keys.C:
+              text += "C";
+              break;
+            case Input.Keys.D:
+              text += "D";
+              break;
+            case Input.Keys.E:
+              text += "E";
+              break;
+            case Input.Keys.F:
+              text += "F";
+              break;
+            case Input.Keys.G:
+              text += "G";
+              break;
+            case Input.Keys.H:
+              text += "H";
+              break;
+            case Input.Keys.I:
+              text += "I";
+              break;
+            case Input.Keys.J:
+              text += "J";
+              break;
+            case Input.Keys.K:
+              text += "K";
+              break;
+            case Input.Keys.L:
+              text += "L";
+              break;
+            case Input.Keys.M:
+              text += "M";
+              break;
+            case Input.Keys.N:
+              text += "N";
+              break;
+            case Input.Keys.O:
+              text += "O";
+              break;
+            case Input.Keys.P:
+              text += "P";
+              break;
+            case Input.Keys.Q:
+              text += "Q";
+              break;
+            case Input.Keys.R:
+              text += "R";
+              break;
+            case Input.Keys.S:
+              text += "S";
+              break;
+            case Input.Keys.T:
+              text += "T";
+              break;
+            case Input.Keys.U:
+              text += "U";
+              break;
+            case Input.Keys.V:
+              text += "V";
+              break;
+            case Input.Keys.W:
+              text += "W";
+              break;
+            case Input.Keys.X:
+              text += "X";
+              break;
+            case Input.Keys.Y:
+              text += "Y";
+              break;
+            case Input.Keys.Z:
+              text += "Z";
+              break;
+          }
+        } else {
+          switch (keyCode) {
+            case Input.Keys.A:
+              text += "a";
+              break;
+            case Input.Keys.B:
+              text += "b";
+              break;
+            case Input.Keys.C:
+              text += "c";
+              break;
+            case Input.Keys.D:
+              text += "d";
+              break;
+            case Input.Keys.E:
+              text += "e";
+              break;
+            case Input.Keys.F:
+              text += "f";
+              break;
+            case Input.Keys.G:
+              text += "g";
+              break;
+            case Input.Keys.H:
+              text += "h";
+              break;
+            case Input.Keys.I:
+              text += "i";
+              break;
+            case Input.Keys.J:
+              text += "j";
+              break;
+            case Input.Keys.K:
+              text += "k";
+              break;
+            case Input.Keys.L:
+              text += "l";
+              break;
+            case Input.Keys.M:
+              text += "m";
+              break;
+            case Input.Keys.N:
+              text += "n";
+              break;
+            case Input.Keys.O:
+              text += "o";
+              break;
+            case Input.Keys.P:
+              text += "p";
+              break;
+            case Input.Keys.Q:
+              text += "q";
+              break;
+            case Input.Keys.R:
+              text += "r";
+              break;
+            case Input.Keys.S:
+              text += "s";
+              break;
+            case Input.Keys.T:
+              text += "t";
+              break;
+            case Input.Keys.U:
+              text += "u";
+              break;
+            case Input.Keys.V:
+              text += "v";
+              break;
+            case Input.Keys.W:
+              text += "w";
+              break;
+            case Input.Keys.X:
+              text += "x";
+              break;
+            case Input.Keys.Y:
+              text += "y";
+              break;
+            case Input.Keys.Z:
+              text += "z";
+              break;
+          }
+        }
+
         switch (keyCode) {
-          case Input.Keys.A:
-            text += "A";
+          case Input.Keys.SPACE:
+            text += " ";
             break;
-          case Input.Keys.B:
-            text += "B";
+          case Input.Keys.COMMA:
+            text += ",";
             break;
-          case Input.Keys.C:
-            text += "C";
+          case Input.Keys.PERIOD:
+            text += ".";
             break;
-          case Input.Keys.D:
-            text += "D";
+          case Input.Keys.NUM_0:
+          case Input.Keys.NUMPAD_0:
+            text += "0";
             break;
-          case Input.Keys.E:
-            text += "E";
+          case Input.Keys.NUM_1:
+          case Input.Keys.NUMPAD_1:
+            text += "1";
             break;
-          case Input.Keys.F:
-            text += "F";
+          case Input.Keys.NUM_2:
+          case Input.Keys.NUMPAD_2:
+            text += "2";
             break;
-          case Input.Keys.G:
-            text += "G";
+          case Input.Keys.NUM_3:
+          case Input.Keys.NUMPAD_3:
+            text += "3";
             break;
-          case Input.Keys.H:
-            text += "H";
+          case Input.Keys.NUM_4:
+          case Input.Keys.NUMPAD_4:
+            text += "4";
             break;
-          case Input.Keys.I:
-            text += "I";
+          case Input.Keys.NUM_5:
+          case Input.Keys.NUMPAD_5:
+            text += "5";
             break;
-          case Input.Keys.J:
-            text += "J";
+          case Input.Keys.NUM_6:
+          case Input.Keys.NUMPAD_6:
+            text += "6";
             break;
-          case Input.Keys.K:
-            text += "K";
+          case Input.Keys.NUM_7:
+          case Input.Keys.NUMPAD_7:
+            text += "7";
             break;
-          case Input.Keys.L:
-            text += "L";
+          case Input.Keys.NUM_8:
+          case Input.Keys.NUMPAD_8:
+            text += "8";
             break;
-          case Input.Keys.M:
-            text += "M";
-            break;
-          case Input.Keys.N:
-            text += "N";
-            break;
-          case Input.Keys.O:
-            text += "O";
-            break;
-          case Input.Keys.P:
-            text += "P";
-            break;
-          case Input.Keys.Q:
-            text += "Q";
-            break;
-          case Input.Keys.R:
-            text += "R";
-            break;
-          case Input.Keys.S:
-            text += "S";
-            break;
-          case Input.Keys.T:
-            text += "T";
-            break;
-          case Input.Keys.U:
-            text += "U";
-            break;
-          case Input.Keys.V:
-            text += "V";
-            break;
-          case Input.Keys.W:
-            text += "W";
-            break;
-          case Input.Keys.X:
-            text += "X";
-            break;
-          case Input.Keys.Y:
-            text += "Y";
-            break;
-          case Input.Keys.Z:
-            text += "Z";
+          case Input.Keys.NUM_9:
+          case Input.Keys.NUMPAD_9:
+            text += "9";
             break;
         }
-      } else {
-        switch (keyCode) {
-          case Input.Keys.A:
-            text += "a";
-            break;
-          case Input.Keys.B:
-            text += "b";
-            break;
-          case Input.Keys.C:
-            text += "c";
-            break;
-          case Input.Keys.D:
-            text += "d";
-            break;
-          case Input.Keys.E:
-            text += "e";
-            break;
-          case Input.Keys.F:
-            text += "f";
-            break;
-          case Input.Keys.G:
-            text += "g";
-            break;
-          case Input.Keys.H:
-            text += "h";
-            break;
-          case Input.Keys.I:
-            text += "i";
-            break;
-          case Input.Keys.J:
-            text += "j";
-            break;
-          case Input.Keys.K:
-            text += "k";
-            break;
-          case Input.Keys.L:
-            text += "l";
-            break;
-          case Input.Keys.M:
-            text += "m";
-            break;
-          case Input.Keys.N:
-            text += "n";
-            break;
-          case Input.Keys.O:
-            text += "o";
-            break;
-          case Input.Keys.P:
-            text += "p";
-            break;
-          case Input.Keys.Q:
-            text += "q";
-            break;
-          case Input.Keys.R:
-            text += "r";
-            break;
-          case Input.Keys.S:
-            text += "s";
-            break;
-          case Input.Keys.T:
-            text += "t";
-            break;
-          case Input.Keys.U:
-            text += "u";
-            break;
-          case Input.Keys.V:
-            text += "v";
-            break;
-          case Input.Keys.W:
-            text += "w";
-            break;
-          case Input.Keys.X:
-            text += "x";
-            break;
-          case Input.Keys.Y:
-            text += "y";
-            break;
-          case Input.Keys.Z:
-            text += "z";
-            break;
-        }
+
       }
-      
-      switch(keyCode){
-        case Input.Keys.SPACE:
-          text += " ";
-          break;
-        case Input.Keys.COMMA:
-          text += ",";
-          break;
-        case Input.Keys.PERIOD:
-          text += ".";
-          break;
-        case Input.Keys.NUM_0:
-        case Input.Keys.NUMPAD_0:
-          text += "0";
-          break;
-        case Input.Keys.NUM_1:
-        case Input.Keys.NUMPAD_1:
-          text += "1";
-          break;
-        case Input.Keys.NUM_2:
-        case Input.Keys.NUMPAD_2:
-          text += "2";
-          break;
-        case Input.Keys.NUM_3:
-        case Input.Keys.NUMPAD_3:
-          text += "3";
-          break;
-        case Input.Keys.NUM_4:
-        case Input.Keys.NUMPAD_4:
-          text += "4";
-          break;
-        case Input.Keys.NUM_5:
-        case Input.Keys.NUMPAD_5:
-          text += "5";
-          break;
-        case Input.Keys.NUM_6:
-        case Input.Keys.NUMPAD_6:
-          text += "6";
-          break;
-        case Input.Keys.NUM_7:
-        case Input.Keys.NUMPAD_7:
-          text += "7";
-          break;
-        case Input.Keys.NUM_8:
-        case Input.Keys.NUMPAD_8:
-          text += "8";
-          break;
-        case Input.Keys.NUM_9:
-        case Input.Keys.NUMPAD_9:
-          text += "9";
-          break;
-      }
-      
     }
-    
+
     return super.keyDown(keyCode);
   }
 
@@ -271,5 +284,16 @@ public class InputText extends InputAdapter {
       capsEnabled = false;
     }
     return super.keyUp(keyCode);
+  }
+
+  public static class Utils {
+
+    public static String hideText(String s) {
+      String aux = "";
+      for (int i = s.length(); i > 0; --i) {
+        aux += "*";
+      }
+      return aux;
+    }
   }
 }
