9130558db5592865d0d1099c7b6f74ba77b5c1b0
Diego Muñoz Jorquera <munoz.jorquera.diego@alumnat.copernic.cat>
2020-05-11 14:30:44 +0200

    Control blank character's name

--- core/src/com/munozdiego/freenscaelis/utils/UserData.java
+++ core/src/com/munozdiego/freenscaelis/utils/UserData.java
@@ -12,55 +12,67 @@ import com.munozdiego.freenscaelis.models.Personaje;
  * @author diego
  */
 public class UserData {
+
   private static UserData instance;
-          
-  public static UserData getInstance(){
-    if(instance == null)
+
+  public static UserData getInstance() {
+    if (instance == null) {
       instance = new UserData();
+    }
     return instance;
   }
-  
+
   private Personaje[] characters;
   private Personaje current;
-  
-  public UserData(){
+
+  public UserData() {
     characters = new Personaje[3];
   }
-  
-  public Personaje[] getCharacters(){
+
+  public Personaje[] getCharacters() {
     return characters;
   }
-  
-  public void setCharacters(Personaje[] p){
+
+  public void setCharacters(Personaje[] p) {
     characters = p;
   }
-  
-  public void eraseCharacter(int i){
+
+  public void eraseCharacter(int i) {
     characters[i] = null;
   }
-  
-  public boolean addCharacter(Personaje p){
+
+  public boolean addCharacter(Personaje p) {
     boolean done = false;
-    
+    boolean error = false;
     int i = 0;
-    while(i < characters.length && !done){
-      if(characters[i] == null){
-        characters[i] = p;
-        done = true;
+
+    while (i < characters.length && !error) {
+      if (characters[i] != null) {
+        error = characters[i].getName().equals(p.getName());
       }
       i++;
     }
     
+    i = 0;
+    if (!error) {
+      while (i < characters.length && !done) {
+        if (characters[i] == null) {
+          characters[i] = p;
+          done = true;
+        }
+        i++;
+      }
+    }
+
     return done;
   }
-  
-  public void selectCharacter(Personaje p){
+
+  public void selectCharacter(Personaje p) {
     current = p;
   }
-  
-  public Personaje getCurrentCharacter(){
+
+  public Personaje getCurrentCharacter() {
     return current;
   }
-  
-  
+
 }
